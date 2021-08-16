448b5855a1ae979f05f452c794325006d74a0489
Chris Ozgo <chrisozgo99@gmail.com>
2021-08-16 02:57:44 -0400

    Updates

---
+++ build/precache-manifest.170e1b6d9e99033a6dabfe7f1b550191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708fed84cbad8c8db36fb09d5fb38264",
+    "url": "/admin-website/index.html"
+  },
+  {
+    "revision": "335321406e73b45c804d",
+    "url": "/admin-website/static/css/2.63343480.chunk.css"
+  },
+  {
+    "revision": "cea79dee8467d989184c",
+    "url": "/admin-website/static/css/main.3724b3a2.chunk.css"
+  },
+  {
+    "revision": "335321406e73b45c804d",
+    "url": "/admin-website/static/js/2.1a08a8cd.chunk.js"
+  },
+  {
+    "revision": "320cfb2c602a01f0e5ae5b4aadc50f93",
+    "url": "/admin-website/static/js/2.1a08a8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea79dee8467d989184c",
+    "url": "/admin-website/static/js/main.dd129694.chunk.js"
+  },
+  {
+    "revision": "71588ee21eb7e0d09dd3",
+    "url": "/admin-website/static/js/runtime-main.8af7b9f5.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac054c8eb0d244ef443e996e754cd568.js"
+  "/admin-website/precache-manifest.170e1b6d9e99033a6dabfe7f1b550191.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/admin-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
